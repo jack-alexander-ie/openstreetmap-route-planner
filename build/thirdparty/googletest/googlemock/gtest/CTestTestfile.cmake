9800c6781b9382572595e4391c4614ab1d8154e3
Jack Mason <jackmasonmusic92@gmail.com>
2019-10-05 20:10:03 -0700

    removing build folder

--- build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jack/Desktop/Courses/C++ Nanodegree Projects/openstreetmap-route-planner/thirdparty/googletest/googletest
-# Build directory: /home/jack/Desktop/Courses/C++ Nanodegree Projects/openstreetmap-route-planner/build/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
